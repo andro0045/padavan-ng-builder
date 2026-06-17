9b4adde5c4ad70efc904c7268ddb920bfd7fed5c
andro <cool.fufel@yandex.ru>
2026-06-17 11:13:41 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -2,16 +2,10 @@
 # e.g.:
 # sudo apt install <pkg>
 
-# -----------------------------
-# Копирование изображений
-# -----------------------------
 cp padavan_logo.png loading_bg.png itoggle.png glyphicons-halflings-white.png \
    glyphicons-halflings.png dark-bg.jpg arrow-right.png ajax-loader.gif \
    padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img/
 
-# -----------------------------
-# Копирование CSS файлов
-# -----------------------------
 cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css/
 
 set -e
