6595b771d96b215f42cdaf9ef3fd6585912008d7
andro <cool.fufel@yandex.ru>
2026-07-01 07:47:46 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -8,17 +8,13 @@ cp padavan_logo.png loading_bg.png itoggle.png glyphicons-halflings-white.png \
 
 cp engage.itoggle.css main.css padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/css/
 
-set -e
+#set -e
 
-TARGET_DIR="padavan-ng/trunk/user/zapret/zapret2"
-TMP_DIR="$(mktemp -d)"
-
-unzip -q zapret2.zip -d "$TMP_DIR"
-
-rm -f "$TARGET_DIR/Makefile"
-rm -f "$TARGET_DIR/zapret2-1.0.1.tar.gz"
-
-cp -f "$TMP_DIR/Makefile" "$TARGET_DIR/"
-cp -f "$TMP_DIR/zapret2-1.0.2.tar.gz" "$TARGET_DIR/"
-
-rm -rf "$TMP_DIR"
+#TARGET_DIR="padavan-ng/trunk/user/zapret/zapret2"
+#TMP_DIR="$(mktemp -d)"
+#unzip -q zapret2.zip -d "$TMP_DIR"
+#rm -f "$TARGET_DIR/Makefile"
+#rm -f "$TARGET_DIR/zapret2-1.0.1.tar.gz"
+#cp -f "$TMP_DIR/Makefile" "$TARGET_DIR/"
+#cp -f "$TMP_DIR/zapret2-1.0.2.tar.gz" "$TARGET_DIR/"
+#rm -rf "$TMP_DIR"
